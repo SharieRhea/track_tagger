40333be20881a7d106cb068ce8b2d14c90ef08a4
SharieRhea <sharierhea@gmail.com>
2025-11-07 17:51:23 -0500

    feat: improve dir selection behavior and selection color

--- src/pages/fileselectpage.py
+++ src/pages/fileselectpage.py
@@ -22,30 +22,46 @@ class FileSelectPage(Screen):
         yield self.filetree
         yield Button("continue")
 
-
+    # FIX: there is some weird behavior were selecting a dir or file sometimes takes two clicks
+    # also, need to create subclass of DirectoryTree so that enter does not expand/collapse dirs, only select them
     @on(DirectoryTree.DirectorySelected)
     def on_directorytree_directoryselected(self, event: DirectoryTree.DirectorySelected) -> None:
         # NOTE: i am choosing to not traverse subdirectories here
-
-        # TODO: this needs to select remaining if only some are selected, also the highlight of the directory itself needs to make sense
-        for node in [child for child in event.node.children if not child.allow_expand]:
-            path = Path(str(node.label))
-            if path in self.selected_files:
-                self.selected_files.remove(path)
+        file_nodes = [child for child in event.node.children if not child.allow_expand]
+        if all(Path(str(node.label)) in self.selected_files for node in file_nodes):
+            # all the files in this dir were selected, deselect them all
+            for node in file_nodes:
+                self.selected_files.remove(Path(str(node.label)))
                 styled_label = Text(str(node.label), Style())
-            else:
-                self.selected_files.append(path)
-                styled_label = Text(str(node.label), Style(bgcolor=self.app.current_theme.accent))
-            node.set_label(styled_label)
+                node.set_label(styled_label)
+            # deselect the dir itself
+            styled_label = Text(str(event.node.label), Style())
+            event.node.set_label(styled_label)
+        else:
+            # some (or no) files have already been selected, select the remaining ones
+            for remaining_node in [node for node in file_nodes if node not in self.selected_files]:
+                self.selected_files.append(Path(str(remaining_node.label)))
+                styled_label = Text(str(remaining_node.label), Style(bgcolor=self.app.theme_variables["primary-background"]))
+                remaining_node.set_label(styled_label)
+            # select the dir itself
+            styled_label = Text(str(event.node.label), Style(bgcolor=self.app.theme_variables["primary-background"]))
+            event.node.set_label(styled_label)
 
     @on(DirectoryTree.FileSelected)
     def on_directorytree_fileselected(self, fileselected: DirectoryTree.FileSelected) -> None:
         if fileselected.path in self.selected_files:
             self.selected_files.remove(fileselected.path)
             styled_label = Text(str(fileselected.node.label), Style())
+            # we are removing a file from the selection, this means its parent dir has at least one file unselected
+            # therefore, update so the dir is deselected (if it wasn't already)
+            dir_node = fileselected.node.parent
+            if dir_node:
+                dir_label = Text(str(dir_node.label), Style())
+                dir_node.set_label(dir_label)
         else:
             self.selected_files.append(fileselected.path)
-            styled_label = Text(str(fileselected.node.label), Style(bgcolor=self.app.current_theme.accent))
+            print(self.app.theme_variables)
+            styled_label = Text(str(fileselected.node.label), Style(bgcolor=self.app.theme_variables["primary-background"]))
         fileselected.node.set_label(styled_label)
 
     @on(Button.Pressed)
