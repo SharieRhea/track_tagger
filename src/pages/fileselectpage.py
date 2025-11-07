30fd5fd5cf658e7b3a094cf00d4c19874dd43886
SharieRhea <sharierhea@gmail.com>
2025-11-06 20:33:20 -0500

    feat: progress towards highlighting selected files/dirs

--- src/pages/fileselectpage.py
+++ src/pages/fileselectpage.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from typing import List
+from rich.style import Style
+from rich.text import Text
 from textual import on
 from textual.app import ComposeResult
 from textual.screen import Screen
@@ -23,16 +25,28 @@ class FileSelectPage(Screen):
 
     @on(DirectoryTree.DirectorySelected)
     def on_directorytree_directoryselected(self, event: DirectoryTree.DirectorySelected) -> None:
-        # retrieve only FILES from this directory and add them to the selection
         # NOTE: i am choosing to not traverse subdirectories here
-        for file in event.path.glob("*"):
-            if not Path.is_dir(file):
-                self.selected_files.append(file)
+
+        # TODO: this needs to select remaining if only some are selected, also the highlight of the directory itself needs to make sense
+        for node in [child for child in event.node.children if not child.allow_expand]:
+            path = Path(str(node.label))
+            if path in self.selected_files:
+                self.selected_files.remove(path)
+                styled_label = Text(str(node.label), Style())
+            else:
+                self.selected_files.append(path)
+                styled_label = Text(str(node.label), Style(bgcolor=self.app.current_theme.accent))
+            node.set_label(styled_label)
 
     @on(DirectoryTree.FileSelected)
     def on_directorytree_fileselected(self, fileselected: DirectoryTree.FileSelected) -> None:
-        self.selected_files.append(fileselected.path)
-        # TODO: highlight the selected file
+        if fileselected.path in self.selected_files:
+            self.selected_files.remove(fileselected.path)
+            styled_label = Text(str(fileselected.node.label), Style())
+        else:
+            self.selected_files.append(fileselected.path)
+            styled_label = Text(str(fileselected.node.label), Style(bgcolor=self.app.current_theme.accent))
+        fileselected.node.set_label(styled_label)
 
     @on(Button.Pressed)
     def on_button_pressed(self, _: Button.Pressed) -> None:
