fa90fb3d661eb2f04416d9ada9b758a0ccaf6950
SharieRhea <sharierhea@gmail.com>
2025-11-17 19:14:37 -0500

    feat: add dynamic ListView of filenames

--- src/pages/editpage.py
+++ src/pages/editpage.py
@@ -9,7 +9,15 @@ from textual.binding import Binding
 from textual.containers import Center, Horizontal, Vertical
 from textual.logging import TextualHandler
 from textual.screen import Screen
-from textual.widgets import Footer, Input, Label, SelectionList, Static
+from textual.widgets import (
+    Footer,
+    Input,
+    Label,
+    ListItem,
+    ListView,
+    SelectionList,
+    Static,
+)
 from textual.widgets.selection_list import Selection
 
 from util.config import load_config
@@ -33,11 +41,15 @@ class EditPage(Screen):
 
     def __init__(self, files: List[Path]):
         super().__init__()
-        self.files = files
+        self.file_paths = files
         self.file_index = 0
         self.config = load_config(self.app)
 
     def compose(self) -> ComposeResult:
+        self.filenames_list: ListView = ListView(
+            *[ListItem(Label(file.name)) for file in self.file_paths], id="filenames-list"
+        )
+
         self.filename: Label = Label(id="filename", classes="round-border")
         self.title_input: Input = Input(placeholder="title", id="title", classes="round-border")
         self.title_input.border_title = "track title"
@@ -67,7 +79,8 @@ class EditPage(Screen):
         self.push_data()
 
         with Horizontal(id="editpage-columns"):
-            with Vertical(id="text-fields"):
+            yield self.filenames_list
+            with Vertical(id="left-column"):
                 yield self.filename
                 yield self.title_input
                 yield self.artist_input
@@ -75,7 +88,7 @@ class EditPage(Screen):
                 yield self.album_artist_input
                 yield self.tag_entry
                 yield self.tags_list
-            with Vertical(id="art-fields"):
+            with Vertical(id="right-column"):
                 with Center():
                     yield self.album_art
         yield Footer()
@@ -92,8 +105,8 @@ class EditPage(Screen):
         return data
 
     def push_data(self) -> None:
-        self.filename.update(self.files[self.file_index].name)
-        data = read_metadata(self.files[self.file_index])
+        self.filename.update(self.file_paths[self.file_index].name)
+        data = read_metadata(self.file_paths[self.file_index])
 
         self.title_input.value = data[0]
         self.artist_input.value = data[1]
@@ -101,15 +114,21 @@ class EditPage(Screen):
         self.album_artist_input.value = data[3]
 
         self.album_art_raw = data[4]
-        image = Pixels.from_image(data[4].resize((60, 60)))
+        image = Pixels.from_image(data[4].resize((40, 40)))
         self.album_art.update(image)
-        self.album_art.styles.width = 60
-        self.album_art.styles.height = 30
+        self.album_art.styles.width = 40
+        self.album_art.styles.height = 20
 
         tags: List[str] = data[5]
         self.tags_list.clear_options()
         for tag in tags:
             self.tags_list.add_option(Selection(tag, tag, True))
+
+    @on(ListView.Selected)
+    def on_listview_selected(self, event: ListView.Selected) -> None:
+        self.file_index = event.index
+        self.push_data()
+        self.refresh_bindings()
 
     @on(Input.Submitted)
     def on_input_submitted(self, event: Input.Submitted) -> None:
@@ -125,7 +144,7 @@ class EditPage(Screen):
         self.tag_entry.clear()
 
     def action_write_out(self) -> None:
-        if write_metadata(self.files[self.file_index], self.pull_data()):
+        if write_metadata(self.file_paths[self.file_index], self.pull_data()):
             self.app.notify(
                 "File saved successfully!",
                 severity="information",
@@ -141,11 +160,14 @@ class EditPage(Screen):
         self.file_index += 1
         self.push_data()
         self.refresh_bindings()
+        # self.filenames_list.highlighted_child = self.filenames_list. [self.file_index]
+        self.filenames_list.index = self.file_index
 
     def action_previous(self) -> None:
         self.file_index -= 1
         self.push_data()
         self.refresh_bindings()
+        self.filenames_list.index = self.file_index
 
     def action_search(self) -> None:
         self.refresh_bindings()
@@ -167,6 +189,6 @@ class EditPage(Screen):
             return None
         if action == "previous" and self.file_index == 0:
             return None
-        if action == "next" and self.file_index == len(self.files) - 1:
+        if action == "next" and self.file_index == len(self.file_paths) - 1:
             return None
         return True
